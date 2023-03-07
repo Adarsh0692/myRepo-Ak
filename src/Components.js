eb7c432a0365fdd81b279a4d20513963176ba51b
my-user <my@email.com>
2023-03-07 10:26:13 +0530

    React assignment done

---
+++ src/Components.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export function Component1(){
+    return (
+        <h1>I am heading one... from first Component</h1>
+    )
+}
+export function Component2(){
+    return (
+        <h2>I am heading two... from second Component</h2>
+    )
+}
+export function Component3(){
+    return (
+        <h3>I am heading three... from first Component</h3>
+    )
+}
+export function Component4(){
+    return (
+        <button>Click me!!</button>
+    )
+}
